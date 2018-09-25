9a93154f6f6ae1a94a05b48086d8d91bd6b2008c
Unknown <alexandre.annic1@gmail.com>
2018-09-25 14:41:38 +0200

    Initial version

---
+++ src/demo/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+interface AppProps {
+
+}
+
+class App extends React.Component<AppProps, {}> {
+
+  render() {
+    return (
+      <div>TODO</div>
+    );
+  }
+}
+
+export default App;
---
+++ src/lib/PersistantComponent/PersistantComponent.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import {LocalStorageEntity} from './localstorage';
+import throttle from 'lodash/throttle';
+
+export abstract class PersistantComponent<P = {}, S = {}, SS = any> extends React.Component<P, S, SS> {
+
+  private localStorage: LocalStorageEntity<S>;
+
+  /**
+   * /!|/!|/!|
+   * Constructor must be called in inheriting class to retrieve Object.getPrototypeOf(this).constructor.name
+   * /!|/!|/!|
+   */
+  protected constructor(props: P, name?: string) {
+    super(props);
+    name = name || Object.getPrototypeOf(this).constructor.name;
+    this.localStorage = new LocalStorageEntity<S>('pc_' + name);
+
+  }
+
+  componentWillMount() {
+    const savedState = this.localStorage.load();
+    if (savedState) {
+      this.state = Object.assign(this.state, savedState);
+    }
+  }
+
+  setState<K extends keyof S>(
+    state: ((prevState: Readonly<S>, props: P) => (Pick<S, K> | S | null)) | (Pick<S, K> | S | null),
+    callback?: () => void
+  ): void {
+    super.setState(state, () => {
+      if (callback) callback();
+      this.save();
+    });
+  }
+
+  protected clearPeristantState() {
+    this.localStorage.clear();
+  }
+
+  private save() {
+    throttle(() => {
+      this.localStorage.save(this.state);
+    }, 1000)();
+  }
+}
