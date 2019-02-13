846b90170b8ed55afa51977f50826588eb8faf4b
Alexandre <alexandre.annic1@gmail.com>
2019-02-13 10:36:42 +0100

    FIx throttle in hook

--- src/lib/PersistentState/usePersistentState.ts
+++ src/lib/PersistentState/usePersistentState.ts
@@ -1,12 +1,16 @@
 import {LocalStorageEntity} from '../utils/localStorageApi'
 import {generateId} from '../utils/hash'
-import {Dispatch, SetStateAction, useEffect, useState} from 'react'
+import {Dispatch, SetStateAction, useEffect, useRef, useState} from 'react'
+import throttle from 'lodash.throttle'
 
 export function usePersistentState<S>(initialState: S | (() => S), key?): [S, Dispatch<SetStateAction<S>>, () => void] {
   const initLocalStorage = () => new LocalStorageEntity<S>(generateId(key))
   const [storage] = useState(initLocalStorage)
   const [state, setState] = useState<S>(storage.load() || initialState)
-  useEffect(() => storage.save(state), [state])
+
+  const throttled = useRef(throttle(storage.save, 1000))
+  useEffect(() => throttled.current(state), [state])
+
   return [
     state,
     setState,
