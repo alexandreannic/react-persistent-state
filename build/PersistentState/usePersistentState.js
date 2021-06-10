d4543428708bc96f0a73ef8739f52f1af2861bcf
Alexandre Annic <alexandre.annic1@gmail.com>
2021-06-10 15:45:30 +0200

    1.1.0

--- build/PersistentState/usePersistentState.js
+++ build/PersistentState/usePersistentState.js
@@ -9,10 +9,10 @@ var hash_1 = require("../utils/hash");
 var react_1 = require("react");
 var lodash_throttle_1 = __importDefault(require("lodash.throttle"));
 function usePersistentState(initialState, key) {
+    var _a;
     var initLocalStorage = function () { return new localStorageApi_1.LocalStorageEntity(hash_1.generateId(key)); };
     var storage = react_1.useState(initLocalStorage)[0];
-    console.log('storage.load()', storage.load());
-    var _a = react_1.useState(storage.load() || initialState), state = _a[0], setState = _a[1];
+    var _b = react_1.useState((_a = storage.load()) !== null && _a !== void 0 ? _a : initialState), state = _b[0], setState = _b[1];
     var throttled = react_1.useRef(lodash_throttle_1.default(storage.save, 1000));
     react_1.useEffect(function () { return throttled.current(state); }, [state]);
     return [
