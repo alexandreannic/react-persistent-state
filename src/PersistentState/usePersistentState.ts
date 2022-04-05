f8afb1fafc5b44878a447f0aff75ab9d4d93baca
Alexandre Annic <alexandre.annic1@gmail.com>
2022-04-05 07:26:47 +0200

    revert ssr compat

--- src/PersistentState/usePersistentState.ts
+++ src/PersistentState/usePersistentState.ts
@@ -5,12 +5,7 @@ import throttle from 'lodash.throttle'
 
 export function usePersistentState<S>(initialState: S | (() => S), key?: string): [S, Dispatch<SetStateAction<S>>, () => void] {
   const localStorage = useMemo(() => new LocalStorageEntity<S>(generateId(key)), [])
-  const [state, setState] = useState<S>(initialState)
-
-  useEffect(() => {
-    const storedState = localStorage.load()
-    if (storedState) setState(storedState)
-  }, [])
+  const [state, setState] = useState<S>(localStorage.load() ?? initialState)
   const throttled = useRef(throttle(localStorage.save, 1000))
   useEffect(() => throttled.current(state), [state])
 
