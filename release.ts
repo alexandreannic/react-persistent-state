09833b73f283f18e133b33df13dc36df0e4741af
Alexandre Annic <alexandre.annic1@gmail.com>
2024-08-16 14:14:07 +0300

    Update to React 18.3.1
    ok

--- release.ts
+++ release.ts
@@ -27,11 +27,11 @@ const newversion = process.argv[2] ?? 'patch' as 'patch' | 'minor' | 'major'
 
 const getPackageVersion = () => JSON.parse(fs.readFileSync('package.json', 'utf8')).version
 
-const isOnMainBranch = () => new RegExp(`${config.devBranch}\s*\n*`).test(execSync('git branch --show-current').toString())
+const isOnMainBranch = () => new RegExp(`${config.mainBranch}\s*\n*`).test(execSync('git branch --show-current').toString())
 
 ;(async () => {
   if (!isOnMainBranch()) {
-    console.error(`You must be on branch ${config.devBranch} to publish.`)
+    console.error(`You must be on branch ${config.mainBranch} to publish.`)
   } else if (!gitWorkspaceIsEmpty()) {
     console.error(`Your git status must be clean before to publish.`)
   } else {
@@ -40,10 +40,10 @@ const isOnMainBranch = () => new RegExp(`${config.devBranch}\s*\n*`).test(execSy
     await run(`npm publish`)
     // await run(`git commit -m "Release ${getPackageVersion()}"`)
     await run(`git push`)
-    await run(`git checkout ${config.mainBranch}`)
-    await run(`git merge ${config.devBranch}`)
-    await run(`git push`)
-    await run(`git checkout ${config.devBranch}`)
+    // await run(`git checkout ${config.mainBranch}`)
+    // await run(`git merge ${config.devBranch}`)
+    // await run(`git push`)
+    // await run(`git checkout ${config.devBranch}`)
     console.log(`Successfully published version ${getPackageVersion()} !`)
   }
 })()
